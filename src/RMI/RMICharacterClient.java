9ff380662975dbdbe609855c1428ef8fbb74c858
QuyetBH203 <buihuuquyet203@gmail.com>
2024-12-27 08:06:10 +0700

    chore: java

--- src/RMI/RMICharacterClient.java
+++ src/RMI/RMICharacterClient.java
@@ -4,6 +4,7 @@ import java.net.MalformedURLException;
 import java.rmi.Naming;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
+import java.util.Base64;
 
 public class RMICharacterClient {
     public static void main(String[] args) throws MalformedURLException, NotBoundException, RemoteException {
@@ -12,6 +13,9 @@ public class RMICharacterClient {
         CharacterService characterService = (CharacterService) Naming.lookup("//203.162.10.109/RMICharacterService");
         Object response = characterService.requestCharacter(studentCode,qCode);
         System.out.println(response);
+        String encoded = Base64.getEncoder().encodeToString(response.toString().getBytes());
+        System.out.println(encoded);
+        characterService.submitCharacter(studentCode,qCode,encoded);
 
     }
 }
